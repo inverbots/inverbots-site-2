7cf69644960422915449f06c5df9f22bf425569e
joseacordobac <joseacordobac@gmail.com>
2023-05-07 22:40:05 -0500

    plush

--- next.config.js
+++ next.config.js
@@ -7,11 +7,17 @@ const nextConfig = {
   images: {
     domains: ['inverbots.com']
   },
-  async rewrites () {
+  async headers () {
     return [
       {
+        // matching all API routes
         source: '/api/:path*',
-        destination: 'https://inverbots.com/wp-json/wp/v2/publicaciones:path*'
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+        ]
       }
     ]
   }
