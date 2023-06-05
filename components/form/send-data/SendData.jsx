b6c9b9ed7f0278d05edbec661a9f9f692ae43c79
joseacordobac <joseacordobac@gmail.com>
2023-06-04 23:10:21 -0500

    configuration form

--- components/form/send-data/SendData.jsx
+++ components/form/send-data/SendData.jsx
@@ -1,10 +1,21 @@
+'use client'
+
 export default function sendData (data) {
   console.log(data)
 
-  fetch('https://script.google.com/macros/u/1/s/AKfycbwR0Wmj68xM8krCP8GwMNvrLt_K8WP_nbyga_ZEi2UjlLbibagZPo7UnMVRNUkCJQn_HQ/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbyEkyGJ2Qo_n1y1vhcP7WTl6vul9uLxsgroeC7dyx3nKXjyS_kXohacRsGePZAg_snfiA/exec', {
     method: 'POST',
-    body: JSON.stringify(data)
-  }).then((rest) => rest.json()).then((newData) => {
-    console.log(newData)
-  }).catch((error) => console.log(error))
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'text/plain;charset=utf-8'
+    }
+  }).then(response => {
+    response.text()
+  })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
