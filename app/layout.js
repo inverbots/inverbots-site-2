e0aa682f3f415f2e37958e9a4c763921c2d5113c
joseacordobac <joseacordobac@gmail.com>
2023-06-10 07:41:23 -0500

    Add analitycs

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,64 @@ import Form from '@/components/form/Form'
 import Footer from '@/components/footer/Footer'
 import TextCallAction from '@/components/text-call-to-action/TextCallAction'
 import WhatsappBtn from '@/components/whats-btn/WhatsBtn'
+import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 export default function RootLayout ({ children }) {
   return (
     <html lang='es'>
       <head>
-        <title>Inverbots | La mejor academia de trading </title>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+        <meta name='keywords' content='Inverbots, Mercado de futuros, Inversiones,Robots de traiding, Academia de trading, trader, bolsa de valores' />
+        <meta name='author' content='Inverbots' />
+        <meta name='google-site-verification' content='bPuKNVrJr2DOOOu0fhvQypoVd_yAnFBypnMclDZGbNE' />
+
+        <Script
+          strategy='afterInteractive'
+          src='https://www.googletagmanager.com/gtag/js?id=UA-121855589-1'
+        />
+
+        <Script
+          id='gooogle-analytics'
+          strategy='afterInteractive'
+        >
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-121855589-1');
+          gtag('config', 'AW-746826540');`}
+        </Script>
+
+        {/* Facebook Pixel Code */}
+        <Script
+          id='facebook-analytics'
+          strategy='afterInteractive'
+        >
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '324446205054171'); 
+            fbq('track', 'PageView');
+        
+
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1414034,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+
+        </Script>
+
       </head>
       <body>
         <Header />
@@ -26,6 +78,7 @@ export default function RootLayout ({ children }) {
         </section>
         <WhatsappBtn />
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
--- components/whats-btn/WhatsBtn.jsx
+++ components/whats-btn/WhatsBtn.jsx
@@ -1,14 +1,29 @@
 'use client'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import style from './whatsbtn.module.css'
+import Link from 'next/link'
 
 export default function WhatsappBtn () {
+  const randomSite = () => {
+    const sites = [
+      'https://api.whatsapp.com/send?phone=573127887846&text=¡Hola! Estoy interesado en saber más sobre Inverbots',
+      'https://api.whatsapp.com/send?phone=573222109312&text=¡Hola! Estoy interesado en saber más sobre Inverbots'
+    ]
+
+    const i = parseInt(Math.random() * sites.length)
+    return sites[i]
+  }
+
   return (
-    <div className={style.whatsapp_btn}>
+    <Link
+      target='_blank'
+      href={randomSite()}
+      className={style.whatsapp_btn}
+    >
       <WhatsAppIcon
         sx={{ fontSize: 50 }}
         className={style.icon_content}
       />
-    </div>
+    </Link>
   )
 }
