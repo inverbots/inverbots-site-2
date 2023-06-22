98c80a0e9c223b63091b2d2bdd06885e87eece6e
joseacordobac <joseacordobac@gmail.com>
2023-06-22 11:55:03 -0500

    Featured: site map

--- app/sitemap.jsx
+++ app/sitemap.jsx
@@ -1,10 +1,34 @@
+const fetchSinglePost = () => {
+  return fetch(`https://administrador.inverbots.com/wp-json/wp/v2/posts?per_page=1999&_fields=slug,modified`, { cache: 'no-store' })
+    .then(rest => rest.json())
+}
+
+const fetchSinglePages = () => {
+  return fetch(`https://administrador.inverbots.com/wp-json/wp/v2/pages?per_page=99&_fields=slug,modified`, { cache: 'no-store' })
+    .then(rest => rest.json())
+}
+
+const getMapElements = (list, baseUrl) => {
+  const listObjects = list.map((singlePost, key) => {
+    return { 
+      url: `${baseUrl + singlePost.slug}`,
+      lastModified: singlePost.modified
+    }
+  })
+
+  return listObjects
+}
+
 export default async function sitemap() {
+  const allPost = await fetchSinglePost()
+  const allPages = await fetchSinglePages()
+
   const baseUrl = "http://inverbots.com/";
+  const allPostUrl = getMapElements(allPost, baseUrl)
+  const allPagesUrl = getMapElements(allPages, baseUrl)
 
   return [
-    { url: baseUrl},
-    { url: `${baseUrl}gracias`},
-    { url: `${baseUrl}aprende-a-invertir-con-ayuda-de-robots`},
-    
+    ...allPagesUrl,
+    ...allPostUrl
   ]
 }
