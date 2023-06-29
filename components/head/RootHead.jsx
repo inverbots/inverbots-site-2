00519eb42c2fe5124659899aee3f5b3c70e272aa
joseacordobac <joseacordobac@gmail.com>
2023-06-29 10:40:29 -0500

    Add clarify traker

--- components/head/RootHead.jsx
+++ components/head/RootHead.jsx
@@ -50,7 +50,17 @@ export default function RootHead(props) {
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
 
       </Script>
-      {/* <Yoast slug={props.slug} /> */}
+      <Script type="text/javascript"
+        id='microsft-analytics'
+        strategy='afterInteractive'
+      >
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hrj02ap8dk");`}
+      </Script>
+
     </head>
   )
 }
