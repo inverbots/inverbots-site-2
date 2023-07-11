0665b592263b5913d6d976bf27c19593b7cdeb34
joseacordobac <joseacordobac@gmail.com>
2023-07-11 16:09:15 -0500

    Featured: add facebook pixel

--- components/head/RootHead.jsx
+++ components/head/RootHead.jsx
@@ -34,22 +34,13 @@ export default function RootHead(props) {
           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
           n.queue=[];t=b.createElement(e);t.async=!0;
           t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '324446205054171'); 
-          fbq('track', 'PageView');
-      
-
-        (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:1414034,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+          fbq('init', '792391655723604');
+          fbq('track', 'PageView');`}
 
       </Script>
+      
       <Script type="text/javascript"
         id='microsft-analytics'
         strategy='afterInteractive'
