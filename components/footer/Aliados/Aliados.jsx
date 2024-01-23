7e7748f9f1104d6f58af5e510ee2c418450c570c
jose.cordoba <jose.cordoba@sinapsis.global>
2024-01-23 09:55:10 -0500

    Ninja logos

---
+++ components/footer/Aliados/Aliados.jsx
@@ -0,0 +1,32 @@
+import style from './aliados.module.css'
+import Image from 'next/image'
+
+function Aliados() {
+  return (
+    <div className={style.aliados}>
+      <div className={style.aliados__content}>
+        <h3 className={style.aliados__title}>Aliados</h3>
+        <div className={style.aliados__logos}>
+            <a href='https://ninjatrader.com/' target='_blank' rel="noreferrer">
+                    <Image 
+                    className={style.aliados__logo_ninja} 
+                    src="https://administrador.inverbots.com/wp-content/uploads/2024/01/NinjaTrader_Wordmark_c.webp" alt="Ninja trader e Inverbots" 
+                    width={167} 
+                    height={47} 
+                />
+            </a>
+            <a href='https://kinetick.com/NinjaTrader' target='_blank' rel="noreferrer">
+                <Image 
+                    className={style.aliados__logo_kinecktik} 
+                    src="https://administrador.inverbots.com/wp-content/uploads/2024/01/7e0f4b_b6db5e01e0154a5590d70b2900471076mv2.webp" alt="Ninja trader e Inverbots" 
+                    width={150} 
+                    height={47} 
+                />
+            </a>    
+        </div>
+      </div>
+    </div>    
+  )
+}
+
+export default Aliados
--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import style from './Footer.module.css'
 import TextCallAction from '../text-call-to-action/TextCallAction'
+import Aliados from '../footer/Aliados/Aliados'
 import Form from '../form/Form'
 
 export default function Footer () {
@@ -14,6 +15,7 @@ export default function Footer () {
         />
         <Form />
       </section>
+      <Aliados />
       <footer className={style.content}>
       <p className={style.paragraph}>Exoneración de responsabilidad: Existe riesgo de pérdidas substanciales en los mercados financieros. Utilice solo dinero cuyas pérdidas pueda costear sin poner en juego su seguridad financiera y estilo de vida. Los testimonios que aparecen en Inverbots.com podrían no ser representativos de la experiencia de otros clientes y no es garantía de resultados o éxito futuros. Resultados hipotéticos de rendimiento pueden tener muchas limitaciones inherentes. Nada en esta página es una recomendación para comprar o vender activos en los mercados. Toda la información ha sido obtenida de fuentes consideradas confiables, sin embargo, la precisión de la misma no puede ser asegurada. Inverbots no asume responsabilidades por el resultado de transacciones particulares.</p>
       </footer>
