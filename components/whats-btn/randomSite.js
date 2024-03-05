e03fea42823b6da9cfb5b348e8fbdea1f9342f70
jose.cordoba <jose.cordoba@sinapsis.global>
2024-03-04 19:26:43 -0500

    remove whatsapp number

--- components/whats-btn/randomSite.js
+++ components/whats-btn/randomSite.js
@@ -1,8 +1,6 @@
 const randomSite = (whatsappText) => {
   const sites = [
     `https://api.whatsapp.com/send?phone=573222109312&text=${whatsappText}`,
-    `https://api.whatsapp.com/send?phone=573222109312&text=${whatsappText}`,
-    `https://api.whatsapp.com/send?phone=573127887846&text=${whatsappText}`,
   ];
 
   const shuffledSites = shuffleArray(sites);
