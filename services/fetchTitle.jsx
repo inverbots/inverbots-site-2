ee4b78bb5d4255526d2b73c8425b9c3c476e2223
inverbots <132021914+inverbots@users.noreply.github.com>
2025-03-11 08:30:32 -0400

    Update fetchTitle.jsx
    roblema está en la definición de la función fetchTitle

--- services/fetchTitle.jsx
+++ services/fetchTitle.jsx
@@ -1,7 +1,15 @@
-const fetchTitle = (slug) => {
-  return fetch(`https://inverbots.xyz/wp-json/wp/v2/meta-data?slug=${slug}`, { cache: 'no-store' })
+const fetchTitle = async (slug) => {
+  try {
+    const response = await fetch(`https://inverbots.xyz/wp-json/wp/v2/meta-data?slug=${slug}`, { cache: 'no-store' });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data; // Devuelve los datos obtenidos
+  } catch (error) {
+    console.error('Error fetching title:', error);
+    return null; // Maneja el error apropiadamente, devolviendo null o un valor por defecto
+  }
+};
 
-    .then(rest => rest.json())
-}
-
-export default fetchTitle
+export default fetchTitle;
