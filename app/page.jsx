7ad352ffe362c531fb6ca4e35dda06848690140f
joseacordobac <joseacordobac@gmail.com>
2025-05-20 06:26:38 -0500

    hidden detail

--- app/page.jsx
+++ app/page.jsx
@@ -54,13 +54,13 @@ export default async function HomePage() {
             const { title, descripcion, imagen } = steps
             return (
               <div className={style.card_step} key={key}>
-                <Image
+                {/* <Image
                   src={details}
                   width='150'
                   height='150'
                   alt='details'
                   className={style.card_detail}
-                />
+                /> */}
                 <div className={style.card_info}>
                   <div className={style.card_content}>
                     <span className={style.card_number}>{key + 1}</span>
