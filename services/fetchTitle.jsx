d2a04e20fd1ac65c6b9847a6966c05d1c7a2d78f
inverbots <132021914+inverbots@users.noreply.github.com>
2025-10-23 14:04:45 -0400

    Refactor fetchTitle function for better readability

--- services/fetchTitle.jsx
+++ services/fetchTitle.jsx
@@ -1,14 +1,20 @@
 const fetchTitle = async (slug) => {
   try {
-    const response = await fetch(`https://inverbots.xyz/wp-json/wp/v2/meta-data?slug=${slug}`, { cache: 'no-store' });
+    const response = await fetch(
+      `https://inverbots.xyz/wp-json/wp/v2/meta-data?slug=${slug}`, 
+      { cache: 'no-store' }
+    );
+    
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      console.error(`Error fetching title for ${slug}: ${response.status}`)
+      return null
     }
+    
     const data = await response.json();
-    return data; // Devuelve los datos obtenidos
+    return data;
   } catch (error) {
     console.error('Error fetching title:', error);
-    return null; // Maneja el error apropiadamente, devolviendo null o un valor por defecto
+    return null;
   }
 };
 
