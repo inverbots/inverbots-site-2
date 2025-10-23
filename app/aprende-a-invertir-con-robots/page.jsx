f6b17aab2863391b23dbea6b9aab1785a37e7e2e
inverbots <132021914+inverbots@users.noreply.github.com>
2025-10-23 14:40:09 -0400

    Fix JSONYoast assignment in page.jsx

--- app/aprende-a-invertir-con-robots/page.jsx
+++ app/aprende-a-invertir-con-robots/page.jsx
@@ -25,7 +25,7 @@ try {
   console.error('Error fetching Yoast:', error)
 }
 const titleData = await fetchTitle(slug)
-const JSONYoast = dataSEO.json
+
 
 export const metadata = getMetadata(titleData)
 
